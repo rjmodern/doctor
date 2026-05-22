80583d1cafe20e2e090a7ca643d0aeaf4ca28fe8
rjmodern <rjinfodesk.kbmh@gmail.com>
2026-05-22 06:08:43 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function filterDoctors() {
+    const filterValue = document.getElementById("deptFilter").value.toLowerCase();
+    const rows = document.querySelectorAll(".doctor-row");
+
+    rows.forEach(row => {
+        const specialty = row.getAttribute("data-dept").toLowerCase();
+        if (filterValue === "all" || specialty === filterValue) {
+            row.style.display = "";
+        } else {
+            row.style.display = "none";
+        }
+    });
+}
